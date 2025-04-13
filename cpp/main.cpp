702c44678f7d4d6e078b762988349d3208ec182a
topaz <topaz11100@gmail.com>
2025-04-13 11:25:43 +0900

    1

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,11 +2,14 @@
 #include "bin_search.h"
 #include "mysort.h"
 #include "combi.h"
+#include <bitset>
 
 
 int main()
 {
-    int n = 10, k = 5;
-    cout << bottom_up_combi(n, k);
+    int n = -1, k = 5;
+    string a = to_string(n);
+    bitset<10> bit(stoi(a));
+    cout << a << " " << bit.to_string();
     return 0;
 }
