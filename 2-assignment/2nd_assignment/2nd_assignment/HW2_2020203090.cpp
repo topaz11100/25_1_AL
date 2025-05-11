fb4917e0a9378ab4cfd6a5ce447f8654a5fb8a87
topaz <topaz11100@gmail.com>
2025-05-12 03:05:12 +0900

    1

--- 2-assignment/2nd_assignment/2nd_assignment/HW2_2020203090.cpp
+++ 2-assignment/2nd_assignment/2nd_assignment/HW2_2020203090.cpp
@@ -1,3 +1,8 @@
+/*
+학번 : 2020203090
+이름 : 한용옥
+*/
+
 #include <iostream>
 #include <fstream>
 #include <algorithm>
@@ -14,18 +19,19 @@ using student = pair<int, int>;
 
 void method1(vector<student> s_v, int k)
 {
-    //값이 차이나면 다른 데이터 그룹 간주, diff에 저장
+    //내림차순 정렬
+    ranges::sort(s_v, greater(), &student::second);
+
+    //연속 값의 차이를 diff에 저장
     //가르는 인덱스는 그룹의 끝 원소 인덱스 + 1
     vector<student> diff;
     for (int i = 0; i < s_v.size() - 1; i += 1)
     {
         int s_v_diff = s_v[i].second - s_v[i + 1].second;
-        if (s_v_diff == 0) continue;
         diff.push_back(student{ i + 1, s_v_diff });
     }
 
-    //diff에서 k-1개 그룹으로 병합
-    //상위 k_1개 정렬
+    //그룹 차이 최대화이므로 상위 k_1개 정렬
     int k_1 = k - 1 < diff.size() ? k - 1 : diff.size();
     ranges::partial_sort(diff, diff.begin() + k_1, greater(), &student::second);
 
@@ -34,13 +40,6 @@ void method1(vector<student> s_v, int k)
     for (int i = 0; i < k_1; i += 1)
         end_idx.push_back(diff[i].first);
 
-    //더 필요하면(이 상황은 같은 점수 배열을 나눠야하므로 어디가 분할되던 최대는 불변이다)
-    if (k - 1 > k_1)
-        for (int i = 1; i < s_v.size() && end_idx.size() < k - 1; i += 1)
-            //같은 점수 구간 쪼갬 - 엔드인덱스에 포함 안된 인덱스 넣음
-            if (find(end_idx.begin(), end_idx.end(), i) == end_idx.end())
-                end_idx.push_back(i);
-
     //경계조건 추가, 분할 인덱스 정렬
     end_idx.push_back(0);
     end_idx.push_back(s_v.size());
@@ -48,13 +47,12 @@ void method1(vector<student> s_v, int k)
 
     //결과 생성 및 반환
     int sum = accumulate(diff.begin(), diff.begin() + k_1, 0,
-        [](int acc, const student& p) { return acc + p.second; });
+                         [](int acc, const student& p) { return acc + p.second; });
     //학번 오름차순 정렬
     for (int i = 0; i < end_idx.size() - 1; i += 1)
         ranges::sort(s_v.begin() + end_idx[i],
-            s_v.begin() + end_idx[i + 1],
-            less(), &student::first);
-
+                     s_v.begin() + end_idx[i + 1],
+                     less(), &student::first);
     //출력부
     cout << sum << endl;
     ofstream out{ "Partition1.txt" };
@@ -79,18 +77,19 @@ double var(int start, int end, const vector<long long>& sum, const vector<long l
 
 //메모이제이션
 void method2_recur(vector<vector<double>>& V,
-    int k, int i,
-    vector<vector<int>>& T,
-    const vector<long long>& sum, const vector<long long>& sum2)
+                   int k, int i,
+                   vector<vector<int>>& T,
+                   const vector<long long>& sum, const vector<long long>& sum2)
 {
-    //배열에 있으면 종료
+    //계산되어있으면 바로 종료
     if (V[k][i] >= 0) return;
 
     //점화식
-    double result = numeric_limits<double>::max();
+    double result = numeric_limits<double>::infinity();
     int opt = 0;
     for (int t = k - 1; t < i; t += 1)
     {
+        //점화식 적용전 해당 값이 계산되어야함
         method2_recur(V, k - 1, t, T, sum, sum2);
         double temp = V[k - 1][t] + var(t + 1, i, sum, sum2);
         if (temp <= result)
@@ -115,15 +114,18 @@ void track_opt_idx(const vector<vector<int>>& T, int k, int i, vector<int>& resu
 
 void method2(vector<student> s_v, int k)
 {
+    //내림차순 정렬
+    ranges::sort(s_v, greater(), &student::second);
+
     int size = s_v.size();
-    //분산 누적합 구조
+    //분산 계산 위한 누적합 배열 초기화
     vector<long long> sum(size);
     vector<long long> sum2(size);
     sum[0] = (long long)s_v[0].second;
     sum2[0] = (long long)(s_v[0].second * s_v[0].second);
     for (int i = 1; i < size; i += 1)
     {
-        sum[i] = sum[i - 1] + (long long)s_v[i].second;
+        sum[i]  = sum[i - 1] + (long long)s_v[i].second;
         sum2[i] = sum2[i - 1] + (long long)(s_v[i].second * s_v[i].second);
     }
 
@@ -149,8 +151,8 @@ void method2(vector<student> s_v, int k)
     //학번 오름차순 정렬
     for (int i = 0; i < opt_idx.size() - 1; i += 1)
         ranges::sort(s_v.begin() + opt_idx[i],
-            s_v.begin() + opt_idx[i + 1],
-            less(), &student::first);
+                     s_v.begin() + opt_idx[i + 1],
+                     less(), &student::first);
     //파일 출력
     ofstream out{ "Partition2.txt" };
     for (int i = 0; i < opt_idx.size() - 1; i += 1)
@@ -172,8 +174,6 @@ int main()
         cin >> temp;
         s_v.push_back(pair{ i + 1, temp });
     }
-    //내림차순 정렬
-    ranges::sort(s_v, greater(), &student::second);
 
     //함수 실행
     method1(s_v, k);
